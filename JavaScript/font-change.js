82983e8dbae8fe8d54360c0d0ebde06d28b67ef5
Nadin Kodithuwakku (NK) <nadin.2018430@iit.ac.lk>
2020-02-25 00:47:54 +0530

    new update #2
    1)Added default button to reset font size to default value.
    2)Modified the js code

--- JavaScript/font-change.js
+++ JavaScript/font-change.js
@@ -16,9 +16,19 @@ function decreaseFontSizeBy5px() {
     countClicksDown++;
 }
 
+//setting font size for the default size
+function setDefault() {                                       
+    txt = document.getElementById('font-change');             //div tag id where u want to decrease the font size of your text field
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    txt.style.fontSize = "initial" 
+}
+
 function upClick(){
     if (countClicksUp<3){
         increaseFontSizeBy5px();
+		if(countClicksDown==3){
+			countClicksDown=0;
+		}	
     }else if(countClicksDown==3){
         countClicksDown=0;
     }
@@ -26,6 +36,9 @@ function upClick(){
 function downClick(){
     if (countClicksDown<3){
         decreaseFontSizeBy5px();
+		if(countClicksUp==3){
+			countClicksUp=0;
+		}
     }else if (countClicksUp==3){
         countClicksUp=0;
     }
